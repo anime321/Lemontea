15d7bf028d651bb6a99324c0fb00690bf3b1c0a0
Martino <martinof.28@libero.it>
2021-05-26 16:53:32 +0200

    update

---
+++ Lemontea.Client/Models/Attributes/CodiceFiscaleAttribute.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lemontea.Client.Models.Attributes
+{
+  public class CodiceFiscaleAttribute : ValidationAttribute
+  {
+    public CodiceFiscaleAttribute() { }
+
+    public string GetErrorMessage() =>
+      "Il formato del codice fiscale non è valido.";
+
+    protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+    {
+      var azienda = (Azienda)validationContext.ObjectInstance;
+
+      return ValidationResult.Success;
+    }
+  }
+}
--- Lemontea.Client/Models/Azienda.cs
+++ Lemontea.Client/Models/Azienda.cs
@@ -11,40 +11,53 @@ namespace Lemontea.Client.Models
     public int Id { get; set; }
 
     [Required]
+    [StringLength(50)]
     public string RagioneSociale { get; set; }
 
     [Required]
+    [StringLength(50)]
     public string Indirizzo { get; set; }
 
     [Required]
+    [StringLength(50)]
     public string NCivico { get; set; }
 
     [Required]
+    [DataType(DataType.PostalCode)]
     public int CAP { get; set; }
 
     [Required]
+    [StringLength(50)]
     public string Nazione { get; set; }
 
+    [StringLength(50)]
     public string Provincia { get; set; }
 
     [Required]
+    [StringLength(50)]
     public string Citta { get; set; }
 
+    [DataType(DataType.PhoneNumber)]
     public string Telefono { get; set; }
 
+    [DataType(DataType.PhoneNumber)]
     public string Fax { get; set; }
 
     [Required]
     [EmailAddress]
+    [StringLength(50)]
     public string Email { get; set; }
 
     [Required]
     [Url]
+    [StringLength(50)]
     public string SitoWeb { get; set; }
 
     [Required]
+    [StringLength(11)]
     public string PartitaIVA { get; set; }
 
+    [StringLength(16)]
     public string CodiceFiscale { get; set; }
   }
 }
